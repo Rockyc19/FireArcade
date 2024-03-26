597674f22f3c0d40e5e4853e7dbf05003a6a61ce
Rockyc19 <98386555+Rockyc19@users.noreply.github.com>
2024-03-26 12:35:48 +0100

    b
    f

--- php/ticketaanmaken.php
+++ php/ticketaanmaken.php
@@ -1,20 +1,5 @@
 <?php
-// Establish a database connection
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "firearcade";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-mysqli_set_charset($conn, "utf8");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+include_once 'dbconn.php';
 
     // Prepare SQL statement
     $sql = "INSERT INTO `ticket`(`Omschrijving`, `Type`) VALUES (?, ?)";
